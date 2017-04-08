2749b594af0b8da76ca408d17b81b7206d24256a
robotan0921 <robotan0921@gmail.com>
2017-04-08 15:40:09 +0900

    Fix cron

--- slack-bot/scripts/score.js
+++ slack-bot/scripts/score.js
@@ -7,7 +7,7 @@
 //   hubot score     - スコアのランキングを表示
 
 var table = require('easy-table');
-var cron = require('cron');
+var cronJob = require('cron').cronJob;
 
 module.exports = function(robot) {
   var NIL_MSG = '結果はありません。';
