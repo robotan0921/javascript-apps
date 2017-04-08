86d27b754d8c456f07b239e42178d1974e1b88e4
robotan0921 <robotan0921@gmail.com>
2017-04-08 13:07:18 +0900

    Fix set func

--- slack-bot/scripts/score.js
+++ slack-bot/scripts/score.js
@@ -12,10 +12,10 @@ module.exports = function(robot) {
   var NIL_MSG = '結果はありません。';
 
   robot.hear(/set\s*(\d[:]\d\d)$/i, function(msg) {
-    var time = msg.match[0].trim();
+    var time = msg.match[1].trim();
 
     // msg.send(" 目覚ましを " + time + " にセットしました ");
-    msg.send(" 目覚ましを " + msg.match[0] + msg.match[1] + " にセットしました ");
+    msg.send(" 目覚ましを " + time + " にセットしました ");
   });
 
   robot.hear(/(.+)\s*--$/i, function(msg) {
